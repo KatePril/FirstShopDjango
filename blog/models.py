fe845376ed2cf36fe958ca4fd8b1f8dc7e40db39
KatePril <ket14pril14@gmail.com>
2023-05-27 21:47:56 +0300

    Add database

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Creaсte your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    discription = models.TextField()
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.title}'
+    
+class Category(models.Model):
+    name = models.CharField(max_length=255)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    number = models.CharField(max_length=255)
+    question = models.TextField()
+    answer = models.TextField()
+
+class Network(models.Model):
+    name = models.TextField
