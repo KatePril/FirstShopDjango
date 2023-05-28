d0802ce9955236b2278b58e49d59de31a9aa994a
KatePril <ket14pril14@gmail.com>
2023-05-28 18:26:21 +0300

    Add article sort

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Article
 # Create your views here.
 def blog(request):
-    articles = Article.objects.all()
+    articles = sorted(Article.objects.all(), key=lambda article: article.updated_at, reverse=True)
     return render(request, 'blog/articles.html', {'articles': articles})
 
 def details(request, id):
