c9d704461735cae7c06c7331d8d4141ae51840bc
KatePril <ket14pril14@gmail.com>
2023-06-03 11:20:42 +0300

    Add tags, fix admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from .models import Article, Category
+from .models import Article, Category, Tag
 # Register your models here.
-admin.site.register(Article)
-admin.site.register(Category)
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'text', 'category__name']
+    list_display = ['title', 'category', 'created_at', 'updated_at']
+    list_filter = ['category', 'tags', 'created_at', 'updated_at']
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'slug']
+    list_filter = ['name']
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag)
