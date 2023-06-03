dda236e283cd1760a9b26831ff8bffccd7cb03db
KatePril <ket14pril14@gmail.com>
2023-06-03 21:45:14 +0300

    Add categories

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,13 @@ from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import CommentForm
-from blog.models import Article, Tag
+from blog.models import Article, Tag, Category
 # Create your views here.
 def blog(request):
     articles = sorted(Article.objects.filter(status='active'), key=lambda article: article.updated_at, reverse=True)
     tags = Tag.objects.all()
-    return render(request, 'blog/articles.html', {'articles': articles, 'tags' : tags})
+    categories = Category.objects.all()
+    return render(request, 'blog/articles.html', {'articles': articles, 'tags' : tags, 'categories': categories})
 
 def details(request, slug):
     article = get_object_or_404(Article, slug=slug, status='active')
@@ -21,15 +22,24 @@ def details(request, slug):
     else:
         form = CommentForm()
     tags = Tag.objects.all()
-    return render(request, 'blog/details.html', {'article': article, 'form': form, 'tags' : tags})
+    categories = Category.objects.all()
+    return render(request, 'blog/details.html', {'article': article, 'form': form, 'tags' : tags, 'categories': categories})
 
 def articles_tag_list(request, tag):
     articles = Article.objects.filter(tags__slug=tag, status='active')
     tags = Tag.objects.all()
-    return render(request, 'blog/articles_tag.html', {'articles': articles, 'title': tag, 'tags' : tags})
+    categories = Category.objects.all()
+    return render(request, 'blog/articles_filter.html', {'articles': articles, 'title': tag, 'tags' : tags, 'filter': 'tag', 'categories': categories})
+
+def articles_category_list(request, category):
+    articles = Article.objects.filter(category__slug=category, status='active')
+    tags = Tag.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'blog/articles_filter.html', {'articles': articles, 'title': category, 'tags' : tags, 'filter': 'category', 'categories': categories})
 
 def search(request):
     query = request.GET.get('query', '')
     articles = Article.objects.filter(Q(title__icontains=query) | Q(text__icontains=query) | Q(discription__icontains=query))
     tags = Tag.objects.all()
-    return render(request, 'blog/search.html', {'articles': articles, 'title': 'Search through blog', 'query': query, 'tags' : tags})
+    categories = Category.objects.all()
+    return render(request, 'blog/search.html', {'articles': articles, 'title': 'Search through blog', 'query': query, 'tags' : tags, 'categories': categories})
