4ff1d12d52872e1002e19c9fbd297439073b5e42
KatePril <ket14pril14@gmail.com>
2023-06-03 12:12:53 +0300

    Add comments, fix admin

--- blog/models.py
+++ blog/models.py
@@ -26,3 +26,13 @@ class Tag(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=255, verbose_name='User name')
+    email = models.EmailField(verbose_name='Email')
+    content = models.TextField(verbose_name='Comment')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created on')
+
+    def __str__(self):
+        return f'{self.article.title} - {self.content}'
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from .forms import CommentForm
 from blog.models import Article
 # Create your views here.
 def blog(request):
@@ -7,5 +8,15 @@ def blog(request):
     return render(request, 'blog/articles.html', {'articles': articles})
 
 def details(request, id):
-    article = Article.objects.get(id=id)
-    return render(request, 'blog/details.html', {'article': article})
+    article = get_object_or_404(Article, id=id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.article = article
+            comment.save()
+            return  redirect('details', id=id)
+    else:
+        form = CommentForm()
+    
+    return render(request, 'blog/details.html', {'article': article, 'form': form})
